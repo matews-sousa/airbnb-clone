5eb34c23e345c8b270de0d3b640d50debf57f44d
Mateus Sousa <mateussdamiano@gmail.com>
2023-03-02 16:13:12 -0300

    added stimulus to navbar menu dropdown

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,4 +5,6 @@
 import { application } from "./application"
 
 import HelloController from "./hello_controller"
+import MenuController from "./menu_controller"
 application.register("hello", HelloController)
+application.register("menu", MenuController)
