646375258a8f43287acbe28772e23e9b87f9be80
Mateus Sousa <mateussdamiano@gmail.com>
2023-03-03 14:26:41 -0300

    added name field to user and attached avatar to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
+
+  validates :name, presence: true, length: { maximum: 25 }
+
+  has_one_attached :avatar, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   get 'places/new', to: 'place#new', as: 'new_place'
   post 'places', to: 'place#create'
 
-  devise_for :users
+  devise_for :users, controllers: { 
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
