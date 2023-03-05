60eeb498426b80d161e50ad29c4236037fb678df
Mateus Sousa <mateussdamiano@gmail.com>
2023-03-05 17:49:49 -0300

    fix specs creating customer in stripe when user factory is called, and added more specs to #first_available_date method in place model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
+    before(:create) { |user| user.define_singleton_method(:maybe_set_stripe_customer_id){} }
     id {1}
     name {"Test User"}
     email {"factory@email.com"}
--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -3,17 +3,44 @@ require 'rails_helper'
 RSpec.describe Place, type: :model do
   describe 'instance methods' do
     describe '#first_available_date' do
-      it 'returns the first available date' do
-        user = create(:user)
-        place = create(:place, host: user)
-        reservation = Reservation.create!(
-          checkin: Date.new(2017, 1, 1),
-          checkout: Date.new(2017, 1, 6),
-          guests: 1,
-          place: place,
-          user: user
-        )
-        expect(place.first_available_date).to eq(Date.new(2017, 1, 7))
+      context 'when there are no reservations' do
+        it 'returns the todays date' do
+          user = create(:user)
+          place = create(:place, host: user)
+          expect(place.first_available_date).to eq(Date.today)
+        end
+      end
+
+      context 'when there are reservations with pending status' do
+        it 'returns the todays date' do
+          user = create(:user)
+          place = create(:place, host: user)
+          reservation = Reservation.create!(
+            checkin: Date.new(2017, 1, 1),
+            checkout: Date.new(2017, 1, 6),
+            guests: 1,
+            status: :pending,
+            place: place,
+            user: user
+          )
+          expect(place.first_available_date).to eq(Date.today)
+        end
+      end
+
+      context 'when there are reservations with paid status' do
+        it 'returns the first available date' do
+          user = create(:user)
+          place = create(:place, host: user)
+          reservation = Reservation.create!(
+            checkin: Date.new(2017, 1, 1),
+            checkout: Date.new(2017, 1, 6),
+            guests: 1,
+            status: :paid,
+            place: place,
+            user: user
+          )
+          expect(place.first_available_date).to eq(Date.new(2017, 1, 7))
+        end
       end
     end
   end
