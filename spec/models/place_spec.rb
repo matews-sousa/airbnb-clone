9251b53c53008ab77763be3345f8178a947521a3
Mateus Sousa <mateussdamiano@gmail.com>
2023-03-07 10:34:28 -0300

    moved place.average_vote spec that was in review_spec.rb to place_spec.rb

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -2,6 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Place, type: :model do
   describe 'instance methods' do
+    describe '#average_vote' do
+      it 'should return the average vote' do
+        user = FactoryBot.create(:user)
+        place = FactoryBot.create(:place)
+  
+        review1 = FactoryBot.create(:review, vote: 1, place: place, user: user)
+        review2 = FactoryBot.create(:review, vote: 5, place: place, user: user)
+        review3 = FactoryBot.create(:review, vote: 3, place: place, user: user)
+  
+        expect(place.average_vote).to eq(3)
+      end
+    end
+
     describe '#first_available_date' do
       context 'when there are no reservations' do
         it 'returns the todays date' do
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,16 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  describe 'class methods' do
-    it 'should return the average vote' do
-      user = FactoryBot.create(:user)
-      place = FactoryBot.create(:place)
-
-      review1 = FactoryBot.create(:review, vote: 1, place: place, user: user)
-      review2 = FactoryBot.create(:review, vote: 5, place: place, user: user)
-      review3 = FactoryBot.create(:review, vote: 3, place: place, user: user)
-
-      expect(place.average_vote).to eq(3)
-    end
-  end
 end
